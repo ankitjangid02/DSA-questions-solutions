e0f3d9347f3981ea292de3b0f56ac289c12075e3
ankitjangid02 <ankitj2811@gmail.com>
2026-06-13 23:30:17 +0530

    add binarySearch with recursion with solving overflow case

---
+++ A2Z/Binary Search/binarySearch(recusive with solving overflow case).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> arr, int low, int high, int target){
+    if(low>high) return -1;
+    int mid = low+(high-low)/2;
+    if(arr[mid]==target) return mid;
+    else if(target>arr[mid]) return binarySearch(arr,mid+1,high,target);
+    return binarySearch(arr,low,mid-1,target);
+}
+
+int main() {
+    vector<int> arr= {3,4,6,7,9,12,17};
+    cout<<binarySearch(arr,0,arr.size(),6);
+
+    return 0;
+}
