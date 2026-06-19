a30de46acde2a9ef1232cf2712b8b54985ded12a
ankitjangid02 <ankitj2811@gmail.com>
2026-06-19 23:08:19 +0530

    add peak2DMatrix

--- A2Z/Binary Search/peak2DMatrix.cpp
+++ A2Z/Binary Search/peak2DMatrix.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int maxCol(vector<vector<int>> &matrix, int col){
-    int n=matrix[col].size();
+    int n=matrix.size();
     int max=-1;
     int index=-1;
     for(int i=0; i<n; i++){
@@ -20,8 +20,8 @@ pair<int,int> peak2DMatrix(vector<vector<int>> &matrix){
     while(low<=high){
         int mid=(low+high)/2;
         int maxRow=maxCol(matrix,mid);
-        int left = mid-1>0 ? matrix[maxRow][mid-1]:-1;
-        int right = mid+1>0 ? matrix[maxRow][mid+1]:-1;
+        int left = mid-1>=0 ? matrix[maxRow][mid-1]:-1;
+        int right = mid+1<m ? matrix[maxRow][mid+1]:-1;
         if(matrix[maxRow][mid]>left && matrix[maxRow][mid]>right) return {maxRow,mid};
         else if(matrix[maxRow][mid]<left) high=mid-1;
         else low=mid+1;
@@ -31,11 +31,10 @@ pair<int,int> peak2DMatrix(vector<vector<int>> &matrix){
 
 int main() {
     vector<vector<int>> matrix={
-        {5,10,8,9,1},
-        {4,25,7,2,5},
-        {3,9,6,28,22},
-        {2,10,11,12,13},
-        {7,8,10,9,19}
+        {48,36,35,17,48},
+        {38,28,38,26,24},
+        {15,9,33,32,6},
+        {49,4,8,10,41}
     };
     
     pair<int,int> p=peak2DMatrix(matrix);
