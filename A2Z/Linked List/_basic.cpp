3a02668098c1f33db7ef5757370259b29368051a
ankitjangid02 <ankitj2811@gmail.com>
2026-06-24 00:43:43 +0530

    added linked list

--- A2Z/Linked List/_basic.cpp
+++ A2Z/Linked List/_basic.cpp
@@ -19,12 +19,26 @@ class Node{
     }
 };
 
+Node* convertArr2LL(vector<int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover =head;
+    for(int i=0; i<arr.size(); i++){
+        Node* temp= new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
 int main() {
-    vector<int> arr={2,5,8,7};
-    Node y=Node(arr[0], nullptr);
-    cout<<y.data;
-    cout<<y.next;
-    Node* x=new Node(arr[0], nullptr);
-    cout<<x->data;
+    vector<int> arr={12,5,8,7};
+    // Node y=Node(arr[0], nullptr);
+    // cout<<y.data;
+    // cout<<y.next;
+    // Node* x=new Node(arr[0], nullptr);
+    // cout<<x->data;
+    
+    Node* head=convertArr2LL(arr);
+    cout<<head->data;
     return 0;
 }
