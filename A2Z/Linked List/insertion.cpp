cfdbe72b113294268aed65f2d68196d164899017
ankitjangid02 <ankitj2811@gmail.com>
2026-06-25 00:20:09 +0530

    add deletion(removeEl)

---
+++ A2Z/Linked List/insertion.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+  public:
+  int data;
+  Node* next;
+  public:
+  Node(int data1){
+      data=data1;
+      next=nullptr;
+  }
+  public:
+  Node(int data1, Node* next1){
+      data=data1;
+      next=next1;
+  }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* temp=head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+// insert at head
+Node* insertHead(Node* head, int val){
+    Node* temp=new Node(val,head);
+    return temp;
+}
+
+int main() {
+    vector<int> arr={12,5,8,7,6,9,10};
+    
+    Node* head=convertArr2LL(arr);
+    print(head);
+    head=insertHead(head, 100);
+    print(head);
+
+    return 0;
+}   
