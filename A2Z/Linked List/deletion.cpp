b3df5c0eb0356289b80c26a7851ba8b38c6b38d3
ankitjangid02 <ankitj2811@gmail.com>
2026-06-24 15:57:15 +0530

    add deletion(deleteTail)

--- A2Z/Linked List/deletion.cpp
+++ A2Z/Linked List/deletion.cpp
@@ -46,12 +46,26 @@ Node* removeHead(Node* head){
     return head;
 }
 
+//delete tail
+Node* deleteTail(Node* head){
+    if(head==NULL || head->next==NULL) return NULL;
+    Node* tail= head;
+    while(tail->next->next){
+        tail=tail->next;
+    }
+    delete tail->next;
+    tail->next=nullptr;
+    return head;
+}
+
 int main() {
     vector<int> arr={12,5,8,7};
     
     Node* head=convertArr2LL(arr);
     print(head);
     head=removeHead(head);
+    print(head);
+    head=deleteTail(head);
     print(head);
 
     return 0;
