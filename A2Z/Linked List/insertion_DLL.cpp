244b99a46f79954e0dd2318457dd4f41fb759bb7
ankitjangid02 <ankitj2811@gmail.com>
2026-06-26 00:32:18 +0530

    add insertion_DLL

---
+++ A2Z/Linked List/insertion_DLL.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    public:
+    Node(int data1, Node* next1, Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+Node* convertArr2DLL(vector<int>& arr){
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1; i<arr.size(); i++){
+        Node* temp=new Node(arr[i], nullptr, prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    Node* tail=head;
+    while(tail){
+        cout<<tail->data<<" ";
+        tail=tail->next;
+    }
+    cout<<endl;
+}
+
+// insert head
+Node* insertHead(Node* head, int val){
+    Node*temp=new Node(val, head, nullptr);
+    head->back=temp;
+    head=temp;
+    return head;
+}
+
+int main() {
+    vector<int> arr={12,5,8,7};
+    Node* head = convertArr2DLL(arr);
+    print(head);
+    head=insertHead(head,10);
+    print(head);
+    
+    return 0;
+}
