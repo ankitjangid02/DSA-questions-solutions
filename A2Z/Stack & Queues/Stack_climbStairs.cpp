5965aa114a143be0ea07c7b06c3cdcafba4fba89
ankitjangid02 <ankitj2811@gmail.com>
2026-07-01 23:23:00 +0530

    add Stack_climbStairs

---
+++ A2Z/Stack & Queues/Stack_climbStairs.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n) {
+    if(n<=2) return n;
+
+    int prev2=1;
+    int prev1=2;
+
+    for(int i=3; i<=n; i++){
+        int curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+
+    return prev1;
+}
+    
+int main() {
+    cout<<climbStairs(4);
+
+    return 0;
+}
