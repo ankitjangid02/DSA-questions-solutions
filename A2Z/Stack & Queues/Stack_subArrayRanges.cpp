288ee3940ef3e3dc635eab23a35b341f7fafa529
ankitjangid02 <ankitj2811@gmail.com>
2026-07-02 22:11:01 +0530

    add Stack_subArrayRanges.cpp

---
+++ A2Z/Stack & Queues/Stack_subArrayRanges.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long subArrayRanges(vector<int>& arr) {
+    int n=arr.size();
+    long long sum=0;
+    
+    for(int i=0; i<n; i++){
+        int largest=arr[i];
+        int smallest=arr[i];
+        
+        for(int j=i+1; j<n; j++){
+            largest=max(largest, arr[j]);
+            smallest=min(smallest, arr[j]);
+            
+            sum+=largest-smallest;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    vector<int> arr={1,4,3,2};
+    cout<<subArrayRanges(arr);
+   
+    
+
+    return 0;
+}
