b220c6770401cbeeaad85b5e3b7f775fcc865a77
ankitjangid02 <ankitj2811@gmail.com>
2026-07-04 23:56:14 +0530

    add maxDepth.cpp

--- A2Z/Binary Trees/maxDepth.cpp
+++ A2Z/Binary Trees/maxDepth.cpp
@@ -24,7 +24,6 @@ int main() {
     Node* root=new Node(1);
     root->left=new Node(2);
     root->right=new Node(3);
-    // root->left->left=new Node(4);
     root->left->right=new Node(5);
     root->left->right->right=new Node(6);
     root->left->right->right->right=new Node(7);
