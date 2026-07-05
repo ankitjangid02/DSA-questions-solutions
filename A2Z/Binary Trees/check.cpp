644b81418b302ade9a45330aeaa30a78a70f4902
ankitjangid02 <ankitj2811@gmail.com>
2026-07-05 19:24:20 +0530

    add check.cpp

--- A2Z/Binary Trees/check.cpp
+++ A2Z/Binary Trees/check.cpp
@@ -41,8 +41,7 @@ int main() {
     root->left=new Node(2);
     root->right=new Node(3);
     root->left->right=new Node(5);
-    // root->left->right->right=new Node(6);
-    // root->left->right->right->right=new Node(7);
+
     
     cout<<check(root);
     
