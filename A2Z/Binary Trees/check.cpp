829541b050610cfa0f4d16e712422d0816e10ea6
ankitjangid02 <ankitj2811@gmail.com>
2026-07-05 19:24:04 +0530

    add check.cpp

---
+++ A2Z/Binary Trees/check.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data=val;
+        left=right=nullptr;
+    }
+};
+
+int maxDepth(Node* root){
+    if(root==nullptr) return 0;
+    
+    int l=maxDepth(root->left);
+    int r=maxDepth(root->right);
+    
+    return 1+max(l,r);
+}
+
+bool check(Node* root){
+    if(root==nullptr) return true;
+    
+    int l=maxDepth(root->left);
+    int r=maxDepth(root->right);
+    
+    if(abs(l-r)>1) return false;
+    
+    bool left=check(root->left);
+    bool right=check(root->right);
+    
+    if(!left || !right) return false;
+    
+    return true;
+}
+
+int main() {
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->right=new Node(5);
+    // root->left->right->right=new Node(6);
+    // root->left->right->right->right=new Node(7);
+    
+    cout<<check(root);
+    
+    return 0;
+}
