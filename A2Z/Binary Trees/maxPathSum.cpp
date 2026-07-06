87073647539a27082d43f5bdc487fb3c72b1105d
ankitjangid02 <ankitj2811@gmail.com>
2026-07-06 23:35:39 +0530

    add maxPathSum.cpp

--- A2Z/Binary Trees/maxPathSum.cpp
+++ A2Z/Binary Trees/maxPathSum.cpp
@@ -13,11 +13,9 @@ struct Node{
 
 int maxPathDown(Node* node, int &maxi){
     if(node==NULL) return 0;
-
     int left=max(0, maxPathDown(node->left, maxi));
     int right=max(0, maxPathDown(node->right, maxi));
     maxi=max(maxi, left+right+node->data);
-
     return max(left, right)+node->data;
 }
 
