e2d89e45c6ddc6eefece4418e89bf80a5072c867
ankitjangid02 <ankitj2811@gmail.com>
2026-07-08 19:44:27 +0530

    add insertIntoBST.cpp

--- A2Z/Binary Search Trees/insertIntoBST.cpp
+++ A2Z/Binary Search Trees/insertIntoBST.cpp
@@ -12,6 +12,7 @@ struct Node{
 };
 
 Node* insertIntoBST(Node* root, int val){
+    
     if(root==NULL) return new Node(val);
     Node* curr=root;
     while(true){
@@ -39,7 +40,7 @@ void inOrder(Node* root){
     inOrder(root->left);
     cout<<root->data<<" ";
     inOrder(root->right);
-    
+
 }
 
 int main() {
