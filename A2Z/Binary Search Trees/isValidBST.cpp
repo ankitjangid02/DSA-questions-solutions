94fd701aec4f458aab96b8106486e7bdffa7c3fc
ankitjangid02 <ankitj2811@gmail.com>
2026-07-09 21:16:39 +0530

    add isValidBST.cpp

--- A2Z/Binary Search Trees/isValidBST.cpp
+++ A2Z/Binary Search Trees/isValidBST.cpp
@@ -14,8 +14,9 @@ struct Node{
 Node* insertIntoBST(Node* root, int val){
 
     if(root==NULL) return new Node(val);
-    
+
     Node* curr=root;
+    
     while(true){
         if(curr->data<=val){
             if(curr->right!=NULL) curr=curr->right;
