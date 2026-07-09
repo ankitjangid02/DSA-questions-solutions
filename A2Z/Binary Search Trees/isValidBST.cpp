1418d55afd0fc331f76dafec6bbeaf48e6d752aa
ankitjangid02 <ankitj2811@gmail.com>
2026-07-09 21:16:49 +0530

    add isValidBST.cpp

--- A2Z/Binary Search Trees/isValidBST.cpp
+++ A2Z/Binary Search Trees/isValidBST.cpp
@@ -16,7 +16,7 @@ Node* insertIntoBST(Node* root, int val){
     if(root==NULL) return new Node(val);
 
     Node* curr=root;
-    
+
     while(true){
         if(curr->data<=val){
             if(curr->right!=NULL) curr=curr->right;
@@ -33,6 +33,7 @@ Node* insertIntoBST(Node* root, int val){
             }
         }
     }
+    
     return root;
 }
 
