317993c5bbf9c7a78ecd593c66b6afcb661824dd
ankitjangid02 <ankitj2811@gmail.com>
2026-07-10 15:02:12 +0530

    add searchBST.cpp

--- A2Z/Binary Search Trees/searchBST.cpp
+++ A2Z/Binary Search Trees/searchBST.cpp
@@ -12,10 +12,11 @@ struct Node{
 };
 
 Node* searchBST(Node* root, int val){
-    
+
     while(root!=NULL && root->data!=val){
         root=val < root->data ? root->left : root->right;
     }
+    
     return root;
 }
 
