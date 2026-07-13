18194d99b38a60914bbc75e0a8ccf5d12a8b9c49
ankitjangid02 <ankitj2811@gmail.com>
2026-07-14 00:20:48 +0530

    add _LIST_creating_weighted_graph.cpp

---
+++ A2Z/Graph/_LIST_creating_weighted_graph.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>> n>> m;
+    vector<pair<int,int>> adj[n+1];
+    for(int i=0; i<m; i++){
+        int u, v, w;
+        cin>>u>>v;
+        cout<<"weight";
+        cin>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    return 0;
+}
