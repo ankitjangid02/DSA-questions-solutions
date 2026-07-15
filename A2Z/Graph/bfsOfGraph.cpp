0f735ecb3da7efab23a3d7c044f838068b030e8e
ankitjangid02 <ankitj2811@gmail.com>
2026-07-15 22:38:45 +0530

    add bfsOfGraph.cpp

---
+++ A2Z/Graph/bfsOfGraph.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bfsOfGraph(int V, vector<int> adj[]){
+    int vis[V]={0};
+    vis[0]=1;
+    queue<int> q;
+    q.push(0);
+    vector<int> bfs;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        bfs.push_back(node);
+        
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                vis[it]=1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}
+int main() {
+    int V=5;
+    vector<int> adj[V];
+    
+    adj[0]={1,2};
+    adj[1]={0,3};
+    adj[2]={0,4};
+    adj[3]={1};
+    adj[4]={2};
+    
+    vector<int> arr=bfsOfGraph(V, adj);
+    for(int x:arr) cout<<x<<" ";
+    return 0;
+}
