f8eedd722d00ba7f65439d27c15b9ce98fe31992
ankitjangid02 <ankitj2811@gmail.com>
2026-07-20 21:49:15 +0530

    add pivotIndex.cpp

---
+++ A2Z/Arrays/pivotIndex.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pivotIndex(vector<int>& nums){
+    int total=0;
+    for(int x:nums) total+=x;
+    
+    int left=0;
+    for(int i=0; i<nums.size()-1; i++){
+        if(left==total-left-nums[i]) return i;
+        left+=nums[i];
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr={1,7,3,6,5,6};
+    cout<<pivotIndex(arr);
+
+    return 0;
+}
