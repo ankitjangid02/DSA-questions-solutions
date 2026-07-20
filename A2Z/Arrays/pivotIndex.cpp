72d14d19f7272d22ca360d05f88bf3512b5dcbc5
ankitjangid02 <ankitj2811@gmail.com>
2026-07-20 21:50:46 +0530

    add pivotIndex.cpp

--- A2Z/Arrays/pivotIndex.cpp
+++ A2Z/Arrays/pivotIndex.cpp
@@ -15,12 +15,11 @@ int pivotIndex(vector<int>& nums){
             right=right+nums[high];
         }
     }
-    if(low+1==high && left==right) return -1;
-    return low+1;
+    return -1;
 }
 
 int main() {
-    vector<int> arr={2,1,-1};
+    vector<int> arr={1,2,3};
     cout<<pivotIndex(arr);
 
     return 0;
