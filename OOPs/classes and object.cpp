4a5923952452a858dcafd013f2e9d5348241ebfe
ankitjangid02 <ankitj2811@gmail.com>
2026-07-25 21:04:12 +0530

    classes and object.cpp

--- OOPs/classes and object.cpp
+++ OOPs/classes and object.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 class Teacher{
+private:
+    double salary;
 public:
     //properties/attributes
     string name;
     string dept;
     string subject;
-    double salary;
     
     //methods/member functions
     void changeDept(string newDept){
@@ -21,7 +22,7 @@ int main() {
     t1.name="Shradha";
     t1.subject="C++";
     t1.dept="Computer Science";
-    t1.salary=25000;
+    // t1.salary=25000;
     
     cout<<t1.name;
     return 0;
