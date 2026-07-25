6ee6cac2d5f40e149ef0d8c49e74fff3a6b1361f
ankitjangid02 <ankitj2811@gmail.com>
2026-07-25 21:15:27 +0530

    Encapsulation.cpp

---
+++ OOPs/Encapsulation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account{
+private:
+    double balance;
+    string password;    //data hiding
+public:
+    string accountId;
+    string username;
+}
+
+int main() {
+    return 0;
+}
