5f97c12b0f9b54f51f5f595eb5fee1fa1a22431d
ankitjangid02 <ankitj2811@gmail.com>
2026-07-28 23:25:41 +0530

    add hierarchial inehritance.cpp

---
+++ OOPs/hierarchial inehritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person{
+public:
+    string name;
+    int age;
+};
+
+class Student:public Person{
+public:
+    int rollno
+};
+
+class Teacher: public Person{
+public:
+    string subject;
+};
+
+int main() {
+    return 0;
+}
